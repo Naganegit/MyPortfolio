b15072cf8124e1d7b3987ab59cee905bdb81e354
Naganegit <ngsk6fwc@gmail.com>
2019-10-21 12:41:14 +0900

    ディレクトリ構成変更

--- js/obj.js
+++ js/obj.js
@@ -8,14 +8,14 @@ const glo_nav = ["index", "about", "works", "contact"];
 const works = [
 	["sample1" ,"img/dummy.jpg" , "テスト1", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
 	["sample2" ,"img/dummy.jpg" , "テスト2", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample3" ,"img/dummy.jpg" , "テスト3", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample4" ,"img/dummy.jpg" , "テスト4", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample5" ,"img/dummy.jpg" , "テスト5", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample6" ,"img/dummy.jpg" , "テスト6", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample7" ,"img/dummy.jpg" , "テスト7", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample8" ,"img/dummy.jpg" , "テスト8", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample9" ,"img/dummy.jpg" , "テスト9", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
-	["sample10" ,"img/dummy.jpg" , "テスト10", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."]
+	// ["sample3" ,"img/dummy.jpg" , "テスト3", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample4" ,"img/dummy.jpg" , "テスト4", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample5" ,"img/dummy.jpg" , "テスト5", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample6" ,"img/dummy.jpg" , "テスト6", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample7" ,"img/dummy.jpg" , "テスト7", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample8" ,"img/dummy.jpg" , "テスト8", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample9" ,"img/dummy.jpg" , "テスト9", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."],
+	// ["sample10" ,"img/dummy.jpg" , "テスト10", "Sample text. Sample text. Sample text. Sample text. Sample text. Sample text."]
 ];
 
 
